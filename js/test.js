169f7d025e79e36d68c0b3203f303b6506f1695a
liang <466035017@qq.com>
2018-01-31 17:03:22 +0800

    Add files via upload

--- js/test.js
+++ js/test.js
@@ -23,6 +23,7 @@ class relation {
         },
         data
     }) {
+        let me = this
         if (!dom) {
             console.error('请传入dom')
             return
@@ -52,9 +53,29 @@ class relation {
         }
         // 创建舞台
         this.stage = new createjs.Stage(this.canvas)
-        this.shape = new createjs.Shape()
-        this.stage.addChild(this.shape)
         createjs.Ticker.on('tick', this.stage)
+        // 创建动画时间线
+        this.duration = 0
+        this.timeLine = new TimelineMax({
+            repeat: 0,
+            // onRepeat () {
+            onComplete () {
+                // console.log(me.timeLine);
+                // me.stage.clear()
+                // setTimeout(() => {
+                //     console.log('a');
+                //     me.stage.clear()
+                // }, 1000)
+                // me.stage.removeAllChildren()
+                // me.createContent()
+                // me.mouseEvent()
+                // console.log(me.stage, me.timeLine);
+            }
+        })
+        this.animationOp = {
+            imgShowTime: 0.5,
+            lineTime: 0.8
+        }
         this.createContent()
         this.mouseEvent()
     }
@@ -62,7 +83,7 @@ class relation {
     createContent () {
         let me = this
         this.data.point.forEach(p => {
-            me.addNode(p.type, p.pos)
+            me.addNode(p)
         })
         this.data.line.forEach(l => {
             me.drawArrows(...l)
@@ -70,10 +91,25 @@ class relation {
     }
     /**
      * 画箭头
-     * start,end为数组，起止点坐标
+     * from,to 为起止点名称
     */
-    drawArrows (start, end) {
-        let me = this
+    drawArrows (from, to) {
+        let me = this,
+            fromPoint = me.stage.getChildByName(from),
+            toPoint = me.stage.getChildByName(to),
+            start = [fromPoint.x, fromPoint.y],
+            end = [toPoint.x, toPoint.y];
+        if (fromPoint.alpha === 0) {
+            // 图片显示
+            let M1 = new TimelineMax()
+            M1.to(fromPoint, me.animationOp.imgShowTime, {
+                alpha: 1
+            })
+            me.timeLine.add(M1, me.duration);
+            me.duration += me.animationOp.imgShowTime
+        }
+        let shape = new createjs.Shape()
+        this.stage.addChild(shape)
         // 箭头长度
         let headlen = me.option.headlen
         // 箭头角度
@@ -82,30 +118,55 @@ class relation {
             // 弧度制
             angle1 = (angle + me.option.theta) * Math.PI / 180,
             angle2 = (angle - me.option.theta) * Math.PI / 180,
-            topX = headlen * Math.cos(angle1), 
-            topY = headlen * Math.sin(angle1), 
-            botX = headlen * Math.cos(angle2), 
+            topX = headlen * Math.cos(angle1),
+            topY = headlen * Math.sin(angle1),
+            botX = headlen * Math.cos(angle2),
             botY = headlen * Math.sin(angle2),
             sX = start[0] + me.option.imgSize.width / 1.414 * Math.cos(angleR),
             sY = start[1] + me.option.imgSize.height / 1.414 * Math.sin(angleR),
             eX = end[0] - me.option.imgSize.width / 1.414 * Math.cos(angleR),
             eY = end[1] - me.option.imgSize.height / 1.414 * Math.sin(angleR)
-        me.shape.graphics.ss(me.option.lineWidth).s(me.option.lineColor).mt(sX, sY).lt(eX, eY)
-        me.shape.graphics.mt(eX - topX, eY - topY).lt(eX, eY).lt(eX - botX, eY - botY)
+        // 动画画线
+        let L1 = new TimelineMax()
+        let op = { a: 0 }
+        L1.to(op, me.animationOp.lineTime, {
+            a: 1,
+            onUpdate () {
+                shape.graphics.c().ss(me.option.lineWidth).s(me.option.lineColor)
+                .mt(sX, sY).lt(sX + (eX - sX ) * op.a, sY + (eY - sY) * op.a)
+            },
+            onComplete () {
+                shape.graphics.mt(eX - topX, eY - topY).lt(eX, eY).lt(eX - botX, eY - botY)
+            }
+        })
+        me.timeLine.add(L1, me.duration);
+        me.duration += me.animationOp.lineTime
+        // 判断结束点是否已经显示,未显示加入动画
+        if (toPoint.alpha === 0) {
+            // 图片显示
+            let M2 = new TimelineMax()
+            M2.to(toPoint, me.animationOp.imgShowTime, {
+                alpha: 1
+            })
+            me.timeLine.add(M2, me.duration);
+            me.duration += me.animationOp.imgShowTime
+        }
     }
     /**
      * 添加节点
      * 节点类型需在option中配置url路径
      * pos为数组,x、y定位
     */
-    addNode (type, pos) {
+    addNode (d) {
         let me = this
-        let img = new createjs.Bitmap(me.imgUrl[type])
+        let img = new createjs.Bitmap(me.imgUrl[d.type])
         img.set({
+            name: d.name,
             regX: me.option.imgSize.width / 2,
             regY: me.option.imgSize.height / 2,
-            x: pos[0],
-            y: pos[1]
+            x: d.pos[0],
+            y: d.pos[1],
+            alpha: 0
         })
         me.stage.addChild(img)
     }
@@ -140,7 +201,6 @@ class relation {
         })
         // 缩放
         me.dom.addEventListener('mousewheel', e => {
-            // console.log('a');
             if (e.wheelDelta > 0) {
                 me.moveAndZoom.zoom *= me.moveAndZoom.zoomSensitivity
             } else {
@@ -153,12 +213,7 @@ class relation {
             if (me.moveAndZoom.zoom < me.moveAndZoom.minZoom) {
                 me.moveAndZoom.zoom = me.moveAndZoom.minZoom
             }
-            // me.moveAndZoom.regX = me.moveAndZoom.regX * me.moveAndZoom.zoom - e.pageX
-            // me.moveAndZoom.regY = me.moveAndZoom.regY * me.moveAndZoom.zoom - e.pageY
-            // console.log(-me.moveAndZoom.regX - me.moveAndZoom.regX * 2);
             me.stage.set({
-                // regX: me.moveAndZoom.regX * me.moveAndZoom.zoom,
-                // regY: me.moveAndZoom.regY * me.moveAndZoom.zoom,
                 scale: me.moveAndZoom.zoom
             })
         })
